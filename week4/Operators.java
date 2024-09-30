7c0866e02819272c2751a0ca1e9101a9aa3d78b9
gy-host-w <1789859128@qq.com>
2024-09-30 09:02:10 +0800

    Add files via upload

---
+++ week4/Operators.java
@@ -0,0 +1,14 @@
+public class Operators {
+    public static void main(String[] args) {
+        int age = 24;
+        int weight = 140;
+        if( age < 18 ){
+            System.out.println("!(age>18)");
+        }
+        else{
+            System.out.println("age > 18");
+        }
+
+
+    }
+}
